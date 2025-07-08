8c3b6878f73a89cfc6270e82cfc761f8c3de6795
Tranchinh2901 <chinh99078@donga.edu.vn>
2025-07-08 21:10:38 +0700

    configure something

--- server/server.js
+++ server/server.js
@@ -12,8 +12,6 @@ const { chatbot } = require('./utils/Integrate_chatbot');
 require('dotenv').config();
 
 const app = express();
-
-// app.use(express.json())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json({ limit: '10mb' }));
@@ -54,7 +52,7 @@ app.post('/api/chatbot', async (req, res) => {
             answer: answer
         });
     } catch (error) {
-        console.error('Chatbot API error:', error);
+        console.log(error)
         return res.status(500).json({
             success: false,
             message: 'Lỗi xử lý chatbot',
